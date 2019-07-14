27be668b3e5bda62859dec429d815675d52cc98e
Jonathan Riquer <jonriquer@gmail.com>
2019-07-14 17:42:15 -0400

    creating url schema and defining routes in index.js

--- urlShortener/index.js
+++ urlShortener/index.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const connectDb = require('./config/db');
+
 const app = express();
 
+// Connect to Database
+connectDb();
+
 app.use(express.json({ extended: false}));
+
+// Define routes
+app.use('/', require('./routes/index'));
+app.use('/api/url', require('./routes/url'));
 
 const PORT = 5000;
 
